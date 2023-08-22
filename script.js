59ca7de069f76a2b1762081f94659c1f7217d220
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-08-22 17:01:01 +0200

    U2-W3-D2

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", event => {
+  const savedName = localStorage.getItem("PlayerName");
+  if (savedName) {
+    document.getElementById("nameSelected").innerText = savedName;
+  }
+});
+
+let timer = sessionStorage.getItem("lastTimerValue") ? parseInt(sessionStorage.getItem("lastTimerValue")) : 0;
+const h3 = document.getElementById("timer");
+const intervallo = setInterval(() => {
+  timer += 1;
+  h3.innerText = timer;
+  sessionStorage.setItem("lastTimerValue", timer);
+}, 1000);
+
+const savePlayerName = function (event) {
+  event.preventDefault();
+  const playerName = document.getElementById("selectedName").value;
+  localStorage.setItem("PlayerName", playerName);
+  const name = document.getElementById("nameSelected");
+  name.innerText = playerName;
+};
+
+const removePlayerName = function () {
+  localStorage.removeItem("PlayerName");
+  document.getElementById("nameSelected").innerText = "";
+};
